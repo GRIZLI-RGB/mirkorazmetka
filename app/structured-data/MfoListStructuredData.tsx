bf83dad7c016a8bafab54118601348a9cbcb8cd3
GRIZLI-RGB <ek916569@gmail.com>
2025-08-29 22:49:40 +0500

    Fix

--- app/structured-data/MfoListStructuredData.tsx
+++ app/structured-data/MfoListStructuredData.tsx
@@ -1,39 +1,59 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 
 interface Company {
-  name: string;
-  url: string;
-  ratingValue: number;
-  reviewCount: number;
-  position: number;
+	name: string;
+	url: string;
+	ratingValue: number;
+	reviewCount: number;
+	position: number;
 }
 
 interface Props {
-  companies: Company[];
+	companies: Company[];
 }
 
 export const MfoListStructuredData: React.FC<Props> = ({ companies }) => {
-  const data = {
-    "@context": "https://schema.org",
-    "@type": "ItemList",
-    itemListElement: companies.map((company) => ({
-      "@type": "ListItem",
-      position: company.position,
-      url: company.url,
-      name: company.name,
-      "aggregateRating": {
-        "@type": "AggregateRating",
-        ratingValue: company.ratingValue,
-        reviewCount: company.reviewCount,
-      },
-    })),
-  };
+	// const data = {
+	//   "@context": "https://schema.org",
+	//   "@type": "ItemList",
+	//   itemListElement: companies.map((company) => ({
+	//     "@type": "ListItem",
+	//     position: company.position,
+	//     url: company.url,
+	//     name: company.name,
+	//     "aggregateRating": {
+	//       "@type": "AggregateRating",
+	//       ratingValue: company.ratingValue,
+	//       reviewCount: company.reviewCount,
+	//     },
+	//   })),
+	// };
 
-  return (
-    <script
-      type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-    />
-  );
+	const data = {
+		"@context": "https://schema.org",
+		"@type": "ItemList",
+		itemListElement: companies.map((company) => ({
+			"@type": "ListItem",
+			position: company.position,
+			item: {
+				"@type": "Organization",
+				name: company.name,
+				url: company.url,
+				aggregateRating: {
+					"@type": "AggregateRating",
+					ratingValue: company.ratingValue,
+					bestRating: 5,
+					ratingCount: company.reviewCount,
+				},
+			},
+		})),
+	};
+
+	return (
+		<script
+			type="application/ld+json"
+			dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+		/>
+	);
 };
